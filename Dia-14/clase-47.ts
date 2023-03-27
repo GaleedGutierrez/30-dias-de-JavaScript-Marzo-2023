311a7de9d81db11ee39b1ed82836d72beb361b2f
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-03-27 01:30:55 -0300

    :white_check_mark: Clase N°47: Modifica una lista de compras

---
+++ Dia-14/clase-47.ts
@@ -0,0 +1,36 @@
+interface InterfaceList {
+	name: string,
+	price: number
+	quantity?: number
+}
+
+export function processShoppingList (list: InterfaceList[]) {
+	list = list.map(modifyShoppingList);
+
+	const PRICES = list.map((product) => product.price);
+	const TOTAL = PRICES.reduce((total, price) => total + price);
+
+	return TOTAL;
+}
+
+function modifyShoppingList (product: InterfaceList) {
+	const { name, quantity } = product;
+	const IS_OFFER = name.includes('oferta');
+
+	if (IS_OFFER) product.price *= 0.8;
+	if (quantity) product.price *= quantity;
+
+	delete product.quantity;
+
+	return product;
+}
+
+const shoppingList = [
+	{ name: 'pan', price: 20, quantity: 2 },
+	{ name: 'leche', price: 25, quantity: 1 },
+	{ name: 'oferta manzanas', price: 10, quantity: 3 },
+];
+
+const xd = processShoppingList(shoppingList);
+
+console.log(xd);
