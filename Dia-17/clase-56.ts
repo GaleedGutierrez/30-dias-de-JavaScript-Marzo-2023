3ef2f70b41df110542fcceb25da19b2963c3032c
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-03-28 02:48:12 -0300

    :white_check_mark: Clase N°56: Crea un auto usando clases

---
+++ Dia-17/clase-56.ts
@@ -0,0 +1,54 @@
+interface InterfaceCar {
+	brand: string,
+	model: string,
+	year: number,
+	mileage: number,
+	state: boolean
+	turnOn: () => void,
+	turnOff: () => void,
+	drive: (kilometers: number) => void
+}
+
+export class Car implements InterfaceCar {
+	brand: string;
+	model: string;
+	year: number;
+	mileage: number;
+	state: boolean;
+	constructor (
+		brand: string,
+		model: string,
+		year: number,
+		mileage: number,
+	) {
+		this.brand = brand;
+		this.model = model;
+		this.year = year;
+		this.mileage = mileage;
+		this.state = false;
+	}
+
+	turnOn () {
+		this.state = true;
+	}
+
+	turnOff () {
+		this.state = false;
+	}
+
+	drive (kilometers: number) {
+		const { state } = this;
+
+		if (!state) throw new Error('El auto está apagado');
+
+		this.mileage += kilometers;
+
+		return this.mileage;
+	}
+}
+
+const toyota = new Car('Toyota', 'Corolla', 2020, 0);
+
+
+toyota.turnOff();
+toyota.drive(100);
