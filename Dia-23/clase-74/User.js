0e79762b7c3d2c66eaaf60a37845dfe77192019f
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-03-29 23:45:55 -0300

    :white_check_mark: Clase N°74: Proxy en servicio de mensajería

---
+++ Dia-23/clase-74/User.js
@@ -0,0 +1,20 @@
+export class User {
+	name;
+	isLogin = false;
+	constructor (name) {
+		this.name = name;
+		this.isLoggedIn = this.isLoggedIn.bind(this);
+	}
+
+	login () {
+		this.isLogin = true;
+	}
+
+	logout () {
+		this.isLogin = false;
+	}
+
+	isLoggedIn () {
+		return this.isLogin;
+	}
+}
---
+++ Dia-23/clase-74/exercise.ts
@@ -0,0 +1,44 @@
+import { User } from './User.js';
+import { Messages } from './messages.js';
+
+interface IMessagesProxy {
+	messages: Messages,
+	user: User,
+	sendMessage (text: string): void,
+	getHistory (): string[],
+	verifyLogin ():	void
+}
+
+export class MessagesProxy implements IMessagesProxy {
+	constructor (
+		public messages: Messages,
+		public user: User
+	) {
+		this.messages = messages;
+		this.user = user;
+	}
+
+	sendMessage (text: string): void {
+		this.verifyLogin();
+
+		this.messages.sendMessage(text);
+	}
+
+	getHistory (): string[] {
+		this.verifyLogin();
+
+		return this.messages.getHistory();
+	}
+
+	verifyLogin ():	void {
+		const { isLoggedIn } = this.user;
+
+		if (!isLoggedIn()) throw new Error('Usuario no registrado');
+	}
+}
+
+const user = new User('John');
+const messages = new Messages();
+const messagesProxy = new MessagesProxy(messages, user);
+
+console.log(messagesProxy.sendMessage('Hola'));
